42c7c3a96cae21b2e8383a319360cd54438756f0
buddhika <buddhika@svr.pdhs.org>
2012-08-15 18:26:07 +0530

    Letter Finished

--- src/java/gov/sp/health/bean/Menu.java
+++ src/java/gov/sp/health/bean/Menu.java
@@ -32,8 +32,6 @@ public class Menu {
 
     @ManagedProperty(value = "#{sessionController}")
     SessionController sessionController;
-
-    
     MenuModel model;
     String temIx = "";
 
@@ -44,8 +42,6 @@ public class Menu {
     public void setTemIx(String temIx) {
         this.temIx = temIx;
     }
-    
-    
 
     private String getLabel(String key) {
         return new MessageProvider().getValue(key);
@@ -66,24 +62,36 @@ public class Menu {
     public void createMenu() {
         model = new DefaultMenuModel();
 //        model.addSubmenu(cadreSubmenu());
-        
-        
+
+
         MenuItem item;
 
-        
+
 
         item = new MenuItem();
         item.setValue(getLabel("home"));
         item.setUrl("index.xhtml");
         model.addMenuItem(item);
-        
-        if (sessionController.privilege.isBmeView()) model.addSubmenu(biomedSubmenu());
+
+        if (sessionController.privilege.isBmeView()) {
+            model.addSubmenu(biomedSubmenu());
+        }
 //        if (sessionController.privilege.isVehicleView()) model.addSubmenu(transportSubmenu());
-        if (sessionController.privilege.isDemographyView()) model.addSubmenu(demographySubmenu());
-        if (sessionController.privilege.isCaderView()) model.addSubmenu(humanSubmenu());
-        if (sessionController.privilege.isMsView()) model.addSubmenu(medicalSubmenu());
-        if (sessionController.privilege.isInventoryView()) model.addSubmenu(inventorySubmenu());
-        if (sessionController.privilege.isManageAccounts()) model.addSubmenu(adminSubmenu());
+        if (sessionController.privilege.isDemographyView()) {
+            model.addSubmenu(demographySubmenu());
+        }
+        if (sessionController.privilege.isCaderView()) {
+            model.addSubmenu(humanSubmenu());
+        }
+        if (sessionController.privilege.isMsView()) {
+            model.addSubmenu(medicalSubmenu());
+        }
+        if (sessionController.privilege.isInventoryView()) {
+            model.addSubmenu(inventorySubmenu());
+        }
+        if (sessionController.privilege.isManageAccounts()) {
+            model.addSubmenu(adminSubmenu());
+        }
 
     }
 
@@ -94,8 +102,8 @@ public class Menu {
 
         submenu = new Submenu();
         submenu.setLabel(getLabel("hr"));
-        
-        
+
+
 
         item = new MenuItem();
         item.setValue(getLabel("InstitutionTypes"));
@@ -357,82 +365,82 @@ public class Menu {
 
         Submenu editSubmenu = new Submenu();
         editSubmenu.setLabel("Edit");
-        
+
         Submenu medicinesSubmenu = new Submenu();
         medicinesSubmenu.setLabel("Medicines");
 
         item = new MenuItem();
         item.setValue("Medicine Groups");
         item.setUrl("ms_medicine_group.xhtml");
-        medicinesSubmenu.getChildren().add(item);        
+        medicinesSubmenu.getChildren().add(item);
 
         item = new MenuItem();
         item.setValue("Item Categories");
         item.setUrl("ms_item_category.xhtml");
-        medicinesSubmenu.getChildren().add(item);         
-        
+        medicinesSubmenu.getChildren().add(item);
+
         item = new MenuItem();
         item.setValue("Generic Names");
         item.setUrl("ms_vtm.xhtml");
         medicinesSubmenu.getChildren().add(item);
-        
+
         item = new MenuItem();
         item.setValue("Trade Names");
         item.setUrl("ms_atm.xhtml");
         medicinesSubmenu.getChildren().add(item);
-        
+
         item = new MenuItem();
         item.setValue("Item Master");
         item.setUrl("ms_amp.xhtml");
         medicinesSubmenu.getChildren().add(item);
-        
+
         item = new MenuItem();
         item.setValue("Import From Excel");
         item.setUrl("ms_import_items.xhtml");
         medicinesSubmenu.getChildren().add(item);
 
         editSubmenu.getChildren().add(medicinesSubmenu);
-        
-        
+
+
         Submenu insSubmenu = new Submenu();
         insSubmenu.setLabel("Institutions");
-        
+
         item = new MenuItem();
         item.setValue("Units");
         item.setUrl("inventory_unit.xhtml");
         insSubmenu.getChildren().add(item);
-        
+
         item = new MenuItem();
         item.setValue("Suppliers");
         item.setUrl("inventory_supplier.xhtml");
         insSubmenu.getChildren().add(item);
-        
+
         item = new MenuItem();
         item.setValue("Manufacturers");
         item.setUrl("inventory_manufacturer.xhtml");
         insSubmenu.getChildren().add(item);
-        
+
         item = new MenuItem();
         item.setValue("Countries");
         item.setUrl("country.xhtml");
         insSubmenu.getChildren().add(item);
 
-        editSubmenu.getChildren().add(insSubmenu);        
-        
-        
+        editSubmenu.getChildren().add(insSubmenu);
+
+
         submenu.getChildren().add(editSubmenu);
-        
+
 
         item = new MenuItem();
         item.setValue(getLabel("msPurchase"));
         item.setUrl("ms_purchase.xhtml");
         submenu.getChildren().add(item);
 
-                item = new MenuItem();
+        item = new MenuItem();
         item.setValue(getLabel("msReceive"));
         item.setUrl("institutions.xhtml");
         submenu.getChildren().add(item);
-        
+
         item = new MenuItem();
         item.setValue(getLabel("msIssue"));
         item.setUrl("designation_category.xhtml");
@@ -474,96 +482,100 @@ public class Menu {
         submenu = new Submenu();
         submenu.setLabel(getLabel("inventory"));
 
-        
-        
+
+
         Submenu editMenu = new Submenu();
         editMenu.setLabel("Edit");
-        
+
         item = new MenuItem();
         item.setValue("Institution Types");
         item.setUrl("institution_type.xhtml");
         editMenu.getChildren().add(item);
-        
+
         item = new MenuItem();
         item.setValue("Institutions");
         item.setUrl("institutions.xhtml");
         editMenu.getChildren().add(item);
- 
+
         item = new MenuItem();
         item.setValue("Units");
         item.setUrl("inventory_unit.xhtml");
         editMenu.getChildren().add(item);
-               
-        
+
+
 
         item = new MenuItem();
         item.setValue("Locations");
         item.setUrl("inventory_location.xhtml");
         editMenu.getChildren().add(item);
-        
-        
+
+
         item = new MenuItem();
         item.setValue("Persons");
         item.setUrl("person.xhtml");
         editMenu.getChildren().add(item);
-        
-        
+
+
         item = new MenuItem();
         item.setValue("Suppliers");
         item.setUrl("inventory_supplier.xhtml");
         editMenu.getChildren().add(item);
-        
-        
+
+
         item = new MenuItem();
         item.setValue("Manufacturers");
         item.setUrl("inventory_manufacturer.xhtml");
         editMenu.getChildren().add(item);
 
-        
-        
+
+
         item = new MenuItem();
         item.setValue("Countries");
         item.setUrl("country.xhtml");
         editMenu.getChildren().add(item);
-        
-        
+
+
         item = new MenuItem();
         item.setValue("Item Categories");
         item.setUrl("inventory_item_category.xhtml");
         editMenu.getChildren().add(item);
-        
-        
-        
+
+
+
         item = new MenuItem();
         item.setValue("Make");
         item.setUrl("inventory_make.xhtml");
         editMenu.getChildren().add(item);
-        
-        
+
+
         item = new MenuItem();
         item.setValue("Model");
         item.setUrl("inventory_modal.xhtml");
-        editMenu.getChildren().add(item);        
-        
-         item = new MenuItem();
+        editMenu.getChildren().add(item);
+
+        item = new MenuItem();
         item.setValue("Items");
         item.setUrl("inventory_item.xhtml");
         editMenu.getChildren().add(item);
-        
-        
+
+
         item = new MenuItem();
         item.setValue("Import Items From Excel");
         item.setUrl("inventory_import_items.xhtml");
-        editMenu.getChildren().add(item);         
-        
+        editMenu.getChildren().add(item);
+
         submenu.getChildren().add(editMenu);
 
         item = new MenuItem();
         item.setValue("Record Room");
         item.setUrl("record_room.xhtml");
         submenu.getChildren().add(item);
-        
-        
+
+        item = new MenuItem();
+        item.setValue("Post");
+        item.setUrl("post.xhtml");
+        submenu.getChildren().add(item);
+
         item = new MenuItem();
         item.setValue(getLabel("purchase"));
         item.setUrl("inventory_purchase.xhtml");
@@ -574,7 +586,7 @@ public class Menu {
         item.setUrl("inventory_issue.xhtml");
         submenu.getChildren().add(item);
 
-item = new MenuItem();
+        item = new MenuItem();
         item.setValue(getLabel("receiveInventoryItems"));
         item.setUrl("inventory_institution_received_bills.xhtml");
         submenu.getChildren().add(item);
@@ -590,18 +602,18 @@ item = new MenuItem();
         item.setUrl("designation_level.xhtml");
         submenu.getChildren().add(item);
 
-                item = new MenuItem();
+        item = new MenuItem();
         item.setValue("Adjust");
         item.setUrl("inventory_adjust.xhtml");
         submenu.getChildren().add(item);
-        
+
         item = new MenuItem();
         item.setValue(getLabel("reports"));
         item.setUrl("inventory_reports.xhtml");
         submenu.getChildren().add(item);
 
-        
-        
+
+
         return submenu;
     }
 
@@ -617,16 +629,15 @@ item = new MenuItem();
         item.setValue(getLabel("activateAccounts"));
         item.setUrl("activate_users.xhtml");
         submenu.getChildren().add(item);
-        
+
 
         item = new MenuItem();
         item.setValue("Manage Accounts");
         item.setUrl("manage_users.xhtml");
-        submenu.getChildren().add(item);        
+        submenu.getChildren().add(item);
 
         return submenu;
     }
-
 
     public MenuModel getModel() {
         return model;
